230ba491744e78f98cb437e58decb4426c487a1b
00019257wiut <00019257wiut@gmail.com>
2023-10-15 21:33:17 +0500

    task 5 in the week 2 lecture

---
+++ task5.py
@@ -0,0 +1,7 @@
+print("Basically, this program will take a number and tell you if it's odd or even.\n")
+checknum = input("Give me a number. ")
+
+if (int(checknum) % 2 == 0):
+    print("This mf is even bro.")
+else:
+    print("This number's odd, two peas in a pod.")
